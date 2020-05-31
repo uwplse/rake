4e0e8d32170896c93e8c96010b9f49e6041459b9
Maaz <maaz.c10@gmail.com>
2020-05-31 13:50:29 -0700

    First stage of gaussian5x5 encoded

--- benchmarks/hexagon/synthesis/lib/hexagon.rkt
+++ benchmarks/hexagon/synthesis/lib/hexagon.rkt
@@ -1,8 +1,16 @@
 #lang rosette
 
 (require "cpp.rkt")
+(require data/bit-vector)
 
-;; Swizzling instructions
+;; Utility definitions
+(define even-vec car)
+(define odd-vec cdr)
+
+;; Swizzling / casting instructions
+(define (vcombine v1 v2)
+  (cons v2 v1))
+
 (define (vshuffo o e)
   (lambda (i)
     (if
@@ -35,35 +43,92 @@
    (lambda (i)
     (if
      (even? i)
-     ((cdr vdd) (quotient i 2))
-     ((car vdd) (quotient i 2))))
+     ((even-vec vdd) (quotient i 2))
+     ((odd-vec vdd) (quotient i 2))))
    (lambda (i)
     (if
      (even? i)
-     ((cdr vdd) (quotient (+ i 64) 2))
-     ((car vdd) (quotient (+ i 64) 2))))))
+     ((even-vec vdd) (quotient (+ i 64) 2))
+     ((odd-vec vdd) (quotient (+ i 64) 2))))))
+
+(define (vzxt v)
+  (cond
+    [((bitvector 8) (v 0))
+     (cons
+      (lambda (i) (zero-extend (v (* i 2)) (bitvector 16)))
+      (lambda (i) (zero-extend (v (+ (* i 2) 1)) (bitvector 16))))]
+    [((bitvector 16) (v 0))
+     (cons
+      (lambda (i) (zero-extend (v (* i 2)) (bitvector 32)))
+      (lambda (i) (zero-extend (v (+ (* i 2) 1)) (bitvector 32))))]))
 
 ;; Processing instructions
+(define (vadd v1 v2)
+  (cons 
+   (lambda (i)
+     (bvadd
+      (cast (v1 (* i 2)) 'uint8 'int16)
+      (cast (v2 (* i 2)) 'uint8 'int16)))
+   (lambda (i)
+     (bvadd
+      (cast (v1 (+ (* i 2) 1)) 'uint8 'int16)
+      (cast (v2 (+ (* i 2) 1)) 'uint8 'int16)))))
+
+(define (vmpy_acc acc v s)
+  (cons 
+   (lambda (i)
+     (bvadd
+      ((car acc) i)
+      (bvmul
+       (cast (v (* i 2)) 'uint8 'int16)
+       (cast s 'int8 'int16))))
+   (lambda (i)
+     (bvadd
+      ((cdr acc) i)
+      (bvmul
+       (cast (v (+ (* i 2) 1)) 'uint8 'int16)
+       (cast s 'int8 'int16))))))
+
 (define (vmpa_acc acc v1 v2 s1 s2)
   (cons 
    (lambda (i)
-     (+
+     (bvadd
       ((car acc) i)
-      (* (v1 i) s1)
-      (* (v2 i) s2)))
+      (bvmul
+       (cast (v1 (* i 2)) 'uint8 'int16)
+       (cast s1 'int8 'int16))
+      (bvmul
+       (cast (v2 (* i 2)) 'uint8 'int16)
+       (cast s2 'int8 'int16))))
    (lambda (i)
-     (+
+     (bvadd
       ((cdr acc) i)
-      (* (v1 i) s1)
-      (* (v2 i) s2)))))
+      (bvmul
+       (cast (v1 (+ (* i 2) 1)) 'uint8 'int16)
+       (cast s1 'int8 'int16))
+      (bvmul
+       (cast (v2 (+ (* i 2) 1)) 'uint8 'int16)
+       (cast s2 'int8 'int16))))))
 
-(define (vzxt v type)
-  (match type
-    ['int8 (lambda (i) (v i))]
-    ['uint8 (lambda (i) (v i))]
-    ['int16 (lambda (i) (zero-extend (v i) (bitvector 16)))]
-    ['uint16 (lambda (i) (zero-extend (v i) (bitvector 16)))]
-    ['int32 (lambda (i) (zero-extend (v i) (bitvector 32)))]
-    ['uint32 (lambda (i) (zero-extend (v i) (bitvector 32)))]))
+(define (vmpa_acc_2 acc vdd s1 s2)
+  (cons 
+   (lambda (i)
+     (bvadd
+      ((car acc) i)
+      (bvmul
+       (cast ((car vdd) (* i 2)) 'uint8 'int16)
+       (cast s1 'int8 'int16))
+      (bvmul
+       (cast ((cdr vdd) (* i 2)) 'uint8 'int16)
+       (cast s2 'int8 'int16))))
+   (lambda (i)
+     (bvadd
+      ((cdr acc) i)
+      (bvmul
+       (cast ((car vdd) (+ (* i 2) 1)) 'uint8 'int16)
+       (cast s1 'int8 'int16))
+      (bvmul
+       (cast ((cdr vdd) (+ (* i 2) 1)) 'uint8 'int16)
+       (cast s2 'int8 'int16))))))
     
 (provide (all-defined-out))
