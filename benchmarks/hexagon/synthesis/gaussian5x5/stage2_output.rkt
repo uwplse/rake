c1fb260003704b168a8fcd70e5ee708142835127
Maaz Ahmad <maaz.c10@gmail.com>
2020-06-10 01:46:53 -0700

    Saving wip

--- benchmarks/hexagon/synthesis/gaussian5x5/stage2_output.rkt
+++ benchmarks/hexagon/synthesis/gaussian5x5/stage2_output.rkt
@@ -14,67 +14,50 @@
 (define-symbolic output.s0.x.x integer?)
 
 ;; Declare scope vars
-(define-symbolic t126 integer?)
+;(define-symbolic t126 integer?)
 
 ;; Declare list of integer terms
-(define (get-int-term) (choose* (cast (bv 2 32) 'int32 'int16) (cast (bv 3 32) 'int32 'int16)))
+;(define (get-int-term) (choose* (cast (bv 2 32) 'int32 'int16) (cast (bv 3 32) 'int32 'int16)))
 
 ;; Declare list of index terms
-(define (get-index-term) (choose* t126))
+;(define (get-index-term) (choose* t126))
 
 ;; Declare list of buffers
 (define (get-buf-term) (choose* rows))
 
 ;; Define original expression
 (define original-expr
-  (vpacko
-   (vmpyi_acc
-    (vadd
-     (ramp rows 126 1)
-     (vmpyi_acc
-      (vmpyi_acc
-       (ramp rows 130 1)
-       (ramp rows 129 1)
-       (bv 4 8))
-      (ramp rows 128 1)
-      (bv 6 8)))
-    (ramp rows 127  1)
-    (bv 4 8))))
+  ;(vpacko
+   ;(vmpyi-acc
+    ;(vadd
+     ;(ramp rows 126 1)
+     ;(vmpyi-acc
+      (vmpyi-acc
+       (interpret-halide (ramp rows 130 1 128))
+       (interpret-halide (ramp rows 129 1 128))
+       (bv 4 8)))
+      ;(ramp rows 128 1)
+      ;(bv 6 8)))
+    ;(ramp rows 127  1)
+    ;(bv 4 8))))
+
+(define-synthax (hxv-expr depth)
+  #:base (choose
+          (get-from rows))
+  #:else (choose
+          (get-from rows)
+          (vpacko
+           (hxv-expr (- depth 1)))
+          (vadd
+           (hxv-expr (- depth 1))
+           (hxv-expr (- depth 1)))
+          (vmpyi-acc
+           (hxv-expr (- depth 1))
+           (hxv-expr (- depth 1))
+           (?? (bitvector 8)))))
 
 (define synthesized-expr
-  (vpacko
-   (vmpyi_acc
-      (vmpyi_acc
-       (vadd
-        (ramp rows 126 1)
-        (ramp rows 130 1))
-       (vadd
-        (ramp rows 127  1)
-        (ramp rows 129 1))
-       (bv 4 8))
-      (ramp rows 128 1)
-      (bv 6 8))))
-
-(define synthesized-expr-alt
-  (Q6_Vb_vshuffo_VbVb
-   (Q6_Vh_vmpyiacc_VhVhRb
-    (Q6_Vh_vmpyiacc_VhVhRb
-     (Q6_Vh_vadd_VhVh
-      sVsum1
-      sVsum5)
-     sVsum3
-     const6)
-    sVsum2a4
-    const4)
-   (Q6_Vh_vmpyiacc_VhVhRb
-    (Q6_Vh_vmpyiacc_VhVhRb
-     (Q6_Vh_vadd_VhVh
-      sVsum0
-      sVsum4)
-     sVsum2
-     const6)
-    sVsum1a3
-    const4)))
+  (hxv-expr 1))
 
 ;; Verification condition
 (define (bounded-eq? oe se lanes)
@@ -87,8 +70,14 @@
         (assert (eq? (oe i) (se i))))))
 
 ;; Synthesize
-(define sol (synthesize #:forall (list rows t126)
-                        #:guarantee (bounded-eq? original-expr synthesized-expr 2)))
+(define st (current-seconds))
+(define sol (synthesize #:forall (list rows)
+                        #:guarantee (bounded-eq? (interpret original-expr) (interpret synthesized-expr) 1)))
+(define runtime (- (current-seconds) st))
 
 ;; Print solution
-(print sol)
+(error-print-width 10000)
+(println sol)
+(evaluate (interpret synthesized-expr) sol)
+((interpret synthesized-expr) 0)
+(printf "\n\nRuntime in seconds: ~a" runtime)
--- benchmarks/hexagon/synthesis/lib/halide.rkt
+++ benchmarks/hexagon/synthesis/lib/halide.rkt
@@ -1,6 +1,80 @@
 #lang rosette
 
 (require "cpp.rkt")
+(require rosette/lib/match)
+
+;; Define Halide's IR
+
+;; Constructors
+(struct x32 (sca) #:transparent)
+(struct x64 (sca) #:transparent)
+(struct x128 (sca) #:transparent)
+(struct ramp (buf base stride len) #:transparent)
+
+;; Type Casts
+(struct uint8x32 (vec vtype) #:transparent)
+(struct uint16x32 (vec vtype) #:transparent)
+(struct uint32x32 (vec vtype) #:transparent)
+(struct int8x32 (vec vtype) #:transparent)
+(struct int16x32 (vec vtype) #:transparent)
+(struct int32x32 (vec vtype) #:transparent)
+(struct uint8x64 (vec vtype) #:transparent)
+(struct uint16x64 (vec vtype) #:transparent)
+(struct uint32x64 (vec vtype) #:transparent)
+(struct int8x64 (vec vtype) #:transparent)
+(struct int16x64 (vec vtype) #:transparent)
+(struct int32x64 (vec vtype) #:transparent)
+(struct uint8x128 (vec vtype) #:transparent)
+(struct uint16x128 (vec vtype) #:transparent)
+(struct uint32x128 (vec vtype) #:transparent)
+(struct int8x128 (vec vtype) #:transparent)
+(struct int16x128 (vec vtype) #:transparent)
+(struct int32x128 (vec vtype) #:transparent)
+
+;; Operations
+(struct vec-add (v1 v2) #:transparent)
+(struct vec-sub (v1 v2) #:transparent)
+(struct vec-mul (v1 v2) #:transparent)
+(struct vec-sdiv (v1 v2) #:transparent)
+(struct vec-udiv (v1 v2) #:transparent)
+
+(define (interpret p)
+  (match p
+    ;; Constructors
+    [(x32 sca) (lambda (i) (interpret sca))]
+    [(x64 sca) (lambda (i) (interpret sca))]
+    [(x128 sca) (lambda (i) (interpret sca))]
+    [(ramp buf base stride len) (lambda (i) ((interpret buf) (+ (interpret base) (* i (interpret stride)))))]
+
+    ;; Type Casts
+    [(uint8x32 vec vtype) (castvec (interpret vec) vtype 'uint8)]
+    [(uint16x32 vec vtype) (castvec (interpret vec) vtype 'uint16)]
+    [(uint32x32 vec vtype) (castvec (interpret vec) vtype 'uint32)]
+    [(int8x32 vec vtype) (castvec (interpret vec) vtype 'int8)]
+    [(int16x32 vec vtype) (castvec (interpret vec) vtype 'int16)]
+    [(int32x32 vec vtype) (castvec (interpret vec) vtype 'int32)]
+    [(uint8x64 vec vtype) (castvec (interpret vec) vtype 'uint8)]
+    [(uint16x64 vec vtype) (castvec (interpret vec) vtype 'uint16)]
+    [(uint32x64 vec vtype) (castvec (interpret vec) vtype 'uint32)]
+    [(int8x64 vec vtype) (castvec (interpret vec) vtype 'int8)]
+    [(int16x64 vec vtype) (castvec (interpret vec) vtype 'int16)]
+    [(int32x64 vec vtype) (castvec (interpret vec) vtype 'int32)]
+    [(uint8x128 vec vtype) (castvec (interpret vec) vtype 'uint8)]
+    [(uint16x128 vec vtype) (castvec (interpret vec) vtype 'uint16)]
+    [(uint32x128 vec vtype) (castvec (interpret vec) vtype 'uint32)]
+    [(int8x128 vec vtype) (castvec (interpret vec) vtype 'int8)]
+    [(int16x128 vec vtype) (castvec (interpret vec) vtype 'int16)]
+    [(int32x128 vec vtype) (castvec (interpret vec) vtype 'int32)]
+
+    ;; Operations
+    [(vec-add v1 v2) (lambda (i) (bvadd ((interpret v1) i) ((interpret v2) i)))]
+    [(vec-sub v1 v2) (lambda (i) (bvsub ((interpret v1) i) ((interpret v2) i)))]
+    [(vec-mul v1 v2) (lambda (i) (bvmul ((interpret v1) i) ((interpret v2) i)))]
+    [(vec-sdiv v1 v2) (lambda (i) (bvsdiv ((interpret v1) i) ((interpret v2) i)))]
+    [(vec-udiv v1 v2) (lambda (i) (bvudiv ((interpret v1) i) ((interpret v2) i)))]
+
+    ;; Base case
+    [_ p]))
 
 ;; Model vector casts
 (define (castvec val from to)
@@ -38,17 +112,4 @@
        ['int32 val]
        ['uint32 val])]))
 
-;; Model Halide IR
-(define (broadcast val)
-  (lambda (i) val))
-
-(define (ramp input base stride)
-  (lambda (i) (input (+ base (* i stride)))))
-
-(define (vec-add v1 v2)
-  (lambda (i) (bvadd (v1 i) (v2 i))))
-
-(define (vec-mul v1 v2)
-  (lambda (i) (bvmul (v1 i) (v2 i))))
-
-(provide (all-defined-out))
+(provide (except-out (all-defined-out) interpret) (rename-out [interpret interpret-halide]))
