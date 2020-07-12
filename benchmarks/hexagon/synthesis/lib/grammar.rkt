6ed90a9622cb545a2fb9399602643c4246608223
Maaz Ahmad <maaz.c10@gmail.com>
2020-07-12 15:51:09 -0700

    Saving wip

--- benchmarks/hexagon/synthesis/lib/grammar.rkt
+++ benchmarks/hexagon/synthesis/lib/grammar.rkt
@@ -8,11 +8,16 @@
 (require "hexagon.rkt")
 (require "ir.rkt")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; STAGE 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Simplest encoding (deprecated because to slow)
 (define-synthax (hxv-expr-recursive depth)
   #:base (choose
           (gather rows))
   #:else (choose
-          ;(gather rows)
+          (gather rows)
           (vpacko
            (hxv-expr (- depth 1)))
           (vadd
@@ -23,113 +28,112 @@
            (hxv-expr (- depth 1))
            (bvadd (bv 2 8) (?? (bitvector 8))))))
 
-(define (??hvx-instr buffers registers)
-  (define-symbolic* r0_idx integer?)
-  (define-symbolic* r1_idx integer?)
+;; Best encoding
+(define (??hvx-instr registers)
+  (define t0 (apply choose* registers))
+  (define t1 (apply choose* registers))
   (choose*
-   (gather (apply choose* buffers))
-   (list-ref registers r0_idx)
-   (vpacko (list-ref registers r0_idx))
-   (vadd (list-ref registers r0_idx) (list-ref registers r1_idx))
-   (vmpyi-acc (list-ref registers r0_idx) (list-ref registers r1_idx) (?? (bitvector 8)))))
+   t0
+   (swizzle t0)
+   (vpackhi t0)
+   (vadd t0 t1)
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))
+   (vasr-rnd-sat t0 t1 (bv 4 16))))
 
-(define (hxv-expr-linear-dynamic buffers)
-  (define r0 (gather (apply choose* buffers)))
-  (define r1 (??hvx-instr buffers (list r0)))
-  (define r2 (??hvx-instr buffers (list r0 r1)))
-  (define r3 (??hvx-instr buffers (list r0 r1 r2)))
-  (define r4 (??hvx-instr buffers (list r0 r1 r2 r3)))
+(define (??hxv-expr buffers)
+  (define r0 (gather buffers))
+  (define r1 (??hvx-instr (list r0)))
+  (define r2 (??hvx-instr (list r0 r1)))
+  (define r3 (??hvx-instr (list r0 r1 r2)))
+  (define r4 (??hvx-instr (list r0 r1 r2 r3)))
   r4)
 
-;(define (gen-idx-map) (define-symbolic* idxm (~> integer? integer?)) idxm)
-
-(define (??hvx-instr1 buffers registers)
+;; Best encoding (unrolled, useful for testing)
+(define (??hvx-instr1 registers)
   (define t0 (apply choose* registers))
   (define t1 (apply choose* registers))
   (choose
-   ;(gather (apply choose* buffers))
-   t0
-   (swizzle t0)
-   (vpacko t0)
-   (vadd t0 t1)))
-   ;(vmpyi-acc t0 t1 (bvadd (bv 2 8) (?? (bitvector 8))))))
-
-(define (??hvx-instr2 buffers registers)
-  (define t0 (apply choose* registers))
-  (define t1 (apply choose* registers))
-  (choose
-   ;(gather (apply choose* buffers))
-   t0
-   (swizzle t0)
-   (vpacko t0)
-   ;(vadd t0 t1)
-   (vmpyi-acc t0 t1 (bvadd (bv 2 8) (?? (bitvector 8))))))
-
-(define (??hvx-instr3 buffers registers)
-  (define t0 (apply choose* registers))
-  (define t1 (apply choose* registers))
-  (choose
-   ;(gather (apply choose* buffers))
    t0
    (swizzle t0)
    (vpacko t0)
    (vadd t0 t1)
-   (vmpyi-acc t0 t1 (bvadd (bv 2 8) (?? (bitvector 8))))))
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))))
 
-(define (??hvx-instr4 buffers registers)
+(define (??hvx-instr2 registers)
   (define t0 (apply choose* registers))
   (define t1 (apply choose* registers))
   (choose
-   ;(gather (apply choose* buffers))
    t0
    (swizzle t0)
    (vpacko t0)
    (vadd t0 t1)
-   (vmpyi-acc t0 t1 (bvadd (bv 2 8) (?? (bitvector 8))))))
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))))
+
+(define (??hvx-instr3 registers)
+  (define t0 (apply choose* registers))
+  (define t1 (apply choose* registers))
+  (choose
+   t0
+   (swizzle t0)
+   (vpacko t0)
+   (vadd t0 t1)
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))))
+
+(define (??hvx-instr4 registers)
+  (define t0 (apply choose* registers))
+  (define t1 (apply choose* registers))
+  (choose
+   t0
+   (swizzle t0)
+   (vpacko t0)
+   (vadd t0 t1)
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))))
 
 (define (??hvx-instr5 buffers registers)
   (define t0 (apply choose* registers))
   (define t1 (apply choose* registers))
   (choose
-   ;(gather (apply choose* buffers))
    t0
    (swizzle t0)
    (vpacko t0)
    (vadd t0 t1)
-   (vmpyi-acc t0 t1 (bvadd (bv 2 8) (?? (bitvector 8))))))
+   (vmpyi-acc t0 t1 (?? (bitvector 8)))))
 
 (define (??hxv-expr-linear-static buffers)
   (define r0 (gather buffers))
-  (define r1 (??hvx-instr1 buffers (list r0)))
-  (define r2 (??hvx-instr2 buffers (list r0 r1)))
-  (define r3 (??hvx-instr3 buffers (list r0 r1 r2)))
-  (define r4 (??hvx-instr4 buffers (list r0 r1 r2 r3)))
-  r2)
+  (define r1 (??hvx-instr1 (list r0)))
+  (define r2 (??hvx-instr2 (list r0 r1)))
+  (define r3 (??hvx-instr3 (list r0 r1 r2)))
+  (define r4 (??hvx-instr4 (list r0 r1 r2 r3)))
+  r4)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; STAGE 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (??hvx-swizzle-instr vreads registers)
+  (define t0 (apply choose* registers))
+  (define t1 (apply choose* registers))
+  (choose*
+   t0
+   (vcombine t0 t1)
+   (valign t0 t1 (??))))
+
 (define (??hvx-swizzle vreads)
   (define r0 (apply choose* vreads))
+  ;(define r1 (??hvx-swizzle-instr vreads (list r0)))
+  ;(define r2 (??hvx-swizzle-instr vreads (list r0 r1)))
   r0)
 
-(define curr_id -1)
-
-(define (plug-swizzle-grammar expr vreads vecs vec_id)
+(define (plug-swizzle-grammar expr vreads)
   (match expr
-    [(gather opts) (set! curr_id (+ curr_id 1))
-                   (if (eq? curr_id vec_id) (??hvx-swizzle vreads) (serve (hash-ref vecs curr_id)))]
+    [(gather opts) (??hvx-swizzle vreads)]
     [(swizzle vec) (??hvx-swizzle vreads)]
-    [(vadd a b) (vadd (plug-swizzle-grammar a vreads vecs vec_id) (plug-swizzle-grammar b vreads vecs vec_id))]
-    [(vmpyi-acc acc v s) (vmpyi-acc (plug-swizzle-grammar acc vreads vecs vec_id) (plug-swizzle-grammar v vreads vecs vec_id) s)]
+    [(vadd a b) (vadd (plug-swizzle-grammar a vreads) (plug-swizzle-grammar b vreads))]
+    [(vmpyi-acc acc v s) (vmpyi-acc (plug-swizzle-grammar acc vreads) (plug-swizzle-grammar v vreads) s)]
     [_ expr]))
 
-(define (gen-final-sketch vecs vec_id vreads expr)
-  (set! curr_id -1)
-  ;(define live_bufs (extract-live-bufs (hash-ref vecs 0)))
-  ;(define vreads (extract-vec-reads original-expr-post-hvx))
-  ;(define vreads (list (vread c1 0) (vread c1 64) 
-  (plug-swizzle-grammar expr vreads vecs vec_id))
+(define (gen-final-sketch expr vreads)
+  (plug-swizzle-grammar expr vreads))
 
 (provide (all-defined-out))
