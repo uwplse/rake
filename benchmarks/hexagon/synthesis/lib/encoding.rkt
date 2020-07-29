34f4b27a5ea293c20e085dd82916da43b6b73fd9
Maaz Ahmad <maaz.c10@gmail.com>
2020-07-28 17:34:36 -0700

    Added placeholder for encoder that uses swizzleflow to synthesize data movement. Readme added.

---
+++ benchmarks/hexagon/synthesis/lib/encoding.rkt
@@ -0,0 +1,42 @@
+#lang rosette
+
+(require "cpp.rkt")
+(require "hexagon.rkt")
+
+(require rosette/lib/match)
+
+;; Utility function: Generates new algebraic variables
+(define id -1)
+(define (get-new-var)
+  (set! id (add1 id 1))
+  id)
+
+;; Hash-table to map all buffer reads to the algebraic variable that represents it
+(define mem-map (make-hash))
+
+;; Utility function: Generates algebraic vars for each read in a gather node (see line 20)
+(define (create-mem-vars key value)
+  (when (not (hash-has-key? mem-map value))
+    (hash-set! mem-map value (get-new-var))))
+    
+
+(define (encode p)
+  (match p
+    ;; Gather instruction
+    [(gather ht)
+     (begin
+       ;; Abstract all memory reads as unique variables
+       (hash-for-each ht create-mem-vars)
+
+       ;; TODO: Return a vector of algebraic vars
+       ;; I represent vectors as lambdas for my interpreters but you may choose any data-structure
+       )]
+       ;(for ([v]))]
+    
+    ;; Addition (widening)
+    ;[(vadd-w Vu Vv)
+     ;; TODO: Return a vector pair of new algebraic vars, where each var `v_i` represents `upcast(vu_i + vd_i)`
+     
+    [_ p]))
+    
+(provide (except-out (all-defined-out) encode) (rename-out [encode gen-swizzleflow-encoding]))
