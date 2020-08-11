3a55934e5b3046e947d032d481bb87d57e133e21
Maaz Ahmad <maaz.c10@gmail.com>
2020-08-11 14:05:06 -0700

    CPP saturation definitions fixed

--- benchmarks/hexagon/synthesis/lib/cpp.rkt
+++ benchmarks/hexagon/synthesis/lib/cpp.rkt
@@ -132,58 +132,67 @@
     [_ (error "NYI: Casting from type ~a" v)]))
 
 ;; Model C++ Saturation
-(define (MIN_CHAR bw) (match bw [8 (bv #x80 8)] [16 (bv #x80 16)] [32 (bv #x80 32)]))
-(define MAX_CHAR (bv #x7f 8))
-(define MIN_UCHAR (bv #x00 8))
-(define MAX_UCHAR (bv #xff 8))
+(define MIN_CHAR -128)
+(define MAX_CHAR 127)
+(define MIN_UCHAR 0)
+(define MAX_UCHAR 255)
 
-(define MIN_SHORT (bv #x8000 16))
-(define MAX_SHORT (bv #x7fff 16))
-(define MIN_USHORT (bv #x0000 16))
-(define MAX_USHORT (bv #xffff 16))
+(define MIN_SHORT -32768)
+(define MAX_SHORT 32767)
+(define MIN_USHORT 0)
+(define MAX_USHORT 65535)
 
-(define MIN_INT (bv #x80000000 32))
-(define MAX_INT (bv #x7fffffff 32))
-(define MIN_UINT (bv #x00000000 32))
-(define MAX_UINT (bv #xffffffff 32))
+(define MIN_INT -2147483648)
+(define MAX_INT 2147483647)
+(define MIN_UINT 0)
+(define MAX_UINT 4294967295)
 
-;; TODO: Fix these placeholder definitions
 (define (sat8 v)
-  (define outV
-    (match v
-      [(int8_t val) v]
-      [(int16_t val) (if (bvslt val (MIN_CHAR 16)) (MIN_CHAR 8) (if (bvsgt val (MAX_CHAR 16)) (MAX_CHAR 8) (extract 7 0 val)))]
-      [(int32_t val) (if (bvslt val (bv #x80 32)) (bv #x80 8) (if (bvsgt val (bv #x7f 32)) (bv #x7f 8) (extract 7 0 val)))]
-      [(uint8_t val) (if (bvslt val MIN_CHAR) (int8_t MIN_CHAR) (if (bvsgt val MIN_CHAR) (int8_t MAX_CHAR) val))]
-      [(uint16_t val) (if (bvslt val MIN_CHAR) (int8_t MIN_CHAR) (if (bvsgt val MIN_CHAR) (int8_t MAX_CHAR) val))]
-      [(uint32_t val) (if (bvslt val MIN_CHAR) (int8_t MIN_CHAR) (if (bvsgt val MIN_CHAR) (int8_t MAX_CHAR) val))]))
-  (int8_t outV))
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_CHAR)
+      (int8_t (bv MIN_CHAR 8))
+      (if (> val MAX_CHAR)
+          (int8_t (bv MAX_CHAR 8))
+          (int8_t (bv val 8)))))
 
-;(define (sat16 v)
- ; (cond
-  ;  [((bitvector 16) v) (if (bvslt val (bv #x8000 16)) (bv #x8000 16) (if (bvsgt val (bv #x7fff 16)) (bv #x7fff 16) val))]
-   ; [((bitvector 32) v) (if (bvslt val (bv #x8000 32)) (bv #x8000 16) (if (bvsgt val (bv #x7fff 32)) (bv #x7fff 16) (extract 15 0 val)))]))
+(define (sat16 v)
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_SHORT)
+      (int16_t (bv MIN_SHORT 16))
+      (if (> val MAX_SHORT)
+          (int16_t (bv MAX_SHORT 16))
+          (int16_t (bv val 16)))))
 
-;(define (sat32 v)
- ; (match v
-  ;  [(int32_t val) v]
-   ; [(uint32_t val) (if (bvslt val ) (int32_t (bv #x80000000 32)) (if (bvsgt val ) (bv #x7fffffff 32) val))]
-    ;[(int64_t val) (if (bvslt val (bv #x80000000 64)) (bv #x80000000 32) (if (bvsgt val (bv #x7fffffff 64)) (bv #x7fffffff 32) (extract 31 0 val)))]))
+(define (sat32 v)
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_INT)
+      (int32_t (bv MIN_INT 32))
+      (if (> val MAX_INT)
+          (int32_t (bv MAX_INT 32))
+          (int32_t (bv val 32)))))
 
-;(define (satu8 v)
- ; (cond
-  ;  [((bitvector 8) val) (if (bvslt val (bv #x0 8)) (bv #x80 8) (if (bvsgt val (bv #x7f 8)) (bv #x7f 8) val))]
-   ; [((bitvector 16) val) (if (bvslt val (bv #x0 16)) (bv #x80 8) (if (bvsgt val (bv #x7f 16)) (bv #x7f 8) (extract 7 0 val)))]
-    ;[((bitvector 32) val) (if (bvslt val (bv #x0 32)) (bv #x80 8) (if (bvsgt val (bv #x7f 32)) (bv #x7f 8) (extract 7 0 val)))]))
+(define (satu8 v)
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_UCHAR)
+      (uint8_t (bv MIN_UCHAR 8))
+      (if (> val MAX_UCHAR)
+          (uint8_t (bv MAX_UCHAR 8))
+          (uint8_t (bv val 8)))))
 
-;(define (satu16 v)
- ; (cond
-  ;  [((bitvector 16) val) (if (bvslt val (bv #x0 16)) (bv #x8000 16) (if (bvsgt val (bv #x7fff 16)) (bv #x7fff 16) val))]
-   ; [((bitvector 32) val) (if (bvslt val (bv #x0 32)) (bv #x8000 16) (if (bvsgt val (bv #x7fff 32)) (bv #x7fff 16) (extract 15 0 val)))]))
+(define (satu16 v)
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_USHORT)
+      (uint16_t (bv MIN_USHORT 16))
+      (if (> val MAX_USHORT)
+          (uint16_t (bv MAX_USHORT 16))
+          (uint16_t (bv val 16)))))
 
-;(define (satu32 v)
- ; (cond
-  ;  [((bitvector 32) val) (if (bvslt val (bv #x0 32)) (bv #x80000000 32) (if (bvsgt val (bv #x7fffffff 32)) (bv #x7fffffff 32) val))]
-   ; [((bitvector 64) val) (if (bvslt val (bv #x0 64)) (bv #x80000000 32) (if (bvsgt val (bv #x7fffffff 64)) (bv #x7fffffff 32) (extract 31 0 val)))]))
+(define (satu32 v)
+  (define val (if (signed? (type v)) (bitvector->integer (eval v)) (bitvector->natural (eval v))))
+  (if (< val MIN_UINT)
+      (uint32_t (bv MIN_UINT 32))
+      (if (> val MAX_UINT)
+          (uint32_t (bv MAX_UINT 32))
+          (uint32_t (bv val 32)))))
 
 (provide (all-defined-out))
