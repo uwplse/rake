eebc185b608070cf999879e80fce6cc539b329f7
Maaz Ahmad <maaz.c10@gmail.com>
2020-08-26 20:22:55 -0700

    Static analysis
    - Implemented a few basic static analysis on the input IR spec such as the set of operators and constants used.

--- benchmarks/hexagon/synthesis/lib/analysis.rkt
+++ benchmarks/hexagon/synthesis/lib/analysis.rkt
@@ -1,10 +1,12 @@
 #lang rosette
 
 (require "cpp.rkt")
+(require "halide.rkt")
 (require "util.rkt")
 
 (require rosette/lib/match)
 
+;; Extract buffer reads
 (define (extract-buf-reads expr)
   (match expr
     [(int8_t v) (extract-buf-reads v)]
@@ -28,8 +30,258 @@
        [(eq? op bvudiv) (flatten (for/list ([operand operands]) (extract-buf-reads operand)))]
        [(eq? op extract) (flatten (for/list ([operand operands]) (extract-buf-reads operand)))]
        [(eq? op app) (mk-typed-expr expr (var-type (list-ref operands 0)))]
-       [else (error "NYI: ~a" expr)])]
+       [else (error "NYI: extract buffer reads from" expr)])]
      
     [_ (list)]))
 
-(provide (all-defined-out))
+;; Extract the set of operators used in the code
+(define (extract-live-ops expr)
+  (match expr
+    ;; Constructors
+    [(x32 sca) (list)]
+    [(x64 sca) (list)]
+    [(x128 sca) (list)]
+    [(ramp buf base stride len) (list)]
+
+    [(slice_vectors vec base stride len) (append (list 'slice) (extract-live-ops vec))]
+    [(concat_vectors v1 v2) (append (list 'concat) (extract-live-ops v1) (extract-live-ops v2))]
+
+    ;; Type Casts
+    [(uint8x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint16x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint32x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int8x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int16x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int32x32 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint8x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint16x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint32x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int8x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int16x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int32x64 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint8x128 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint16x128 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(uint32x128 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int8x128 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int16x128 vec) (append (list 'cast) (extract-live-ops vec))]
+    [(int32x128 vec) (append (list 'cast) (extract-live-ops vec))]
+
+    ;; Operations
+    [(vec-add v1 v2) (append (list 'add) (extract-live-ops v1) (extract-live-ops v2))]
+    [(vec-sub v1 v2) (append (list 'sub) (extract-live-ops v1) (extract-live-ops v2))]
+    [(vec-div v1 v2) (if (broadcast? v2)
+                         (append (list 'vec-sca-div) (extract-live-ops v1) (extract-live-ops v2))
+                         (append (list 'vec-vec-div) (extract-live-ops v1) (extract-live-ops v2)))]
+    [(vec-mul v1 v2) (if (or (broadcast? v1) (broadcast? v2))
+                         (append (list 'vec-sca-mul) (extract-live-ops v1) (extract-live-ops v2))
+                         (append (list 'vec-vec-mul) (extract-live-ops v1) (extract-live-ops v2)))]
+    
+    ;; Base case
+    [_ (error "Don't know how to get live ops from:" expr)]))
+
+;; Extract constants used with various operators
+(define (extract-add-consts expr)
+  (match expr
+    ;; Constructors
+    [(x32 sca) (list)]
+    [(x64 sca) (list)]
+    [(x128 sca) (list)]
+    [(ramp buf base stride len) (list)]
+
+    [(slice_vectors vec base stride len) (extract-add-consts vec)]
+    [(concat_vectors v1 v2) (append (extract-add-consts v1) (extract-add-consts v2))]
+
+    ;; Type Casts
+    [(uint8x32 vec) (extract-add-consts vec)]
+    [(uint16x32 vec) (extract-add-consts vec)]
+    [(uint32x32 vec) (extract-add-consts vec)]
+    [(int8x32 vec) (extract-add-consts vec)]
+    [(int16x32 vec) (extract-add-consts vec)]
+    [(int32x32 vec) (extract-add-consts vec)]
+    [(uint8x64 vec) (extract-add-consts vec)]
+    [(uint16x64 vec) (extract-add-consts vec)]
+    [(uint32x64 vec) (extract-add-consts vec)]
+    [(int8x64 vec) (extract-add-consts vec)]
+    [(int16x64 vec) (extract-add-consts vec)]
+    [(int32x64 vec) (extract-add-consts vec)]
+    [(uint8x128 vec) (extract-add-consts vec)]
+    [(uint16x128 vec) (extract-add-consts vec)]
+    [(uint32x128 vec) (extract-add-consts vec)]
+    [(int8x128 vec) (extract-add-consts vec)]
+    [(int16x128 vec) (extract-add-consts vec)]
+    [(int32x128 vec) (extract-add-consts vec)]
+
+    ;; Operations
+    [(vec-add v1 v2) (append
+                      (if (broadcast? v1) (extract-consts v1) (extract-add-consts v1))
+                      (if (broadcast? v2) (extract-consts v2) (extract-add-consts v2)))]
+    [(vec-sub v1 v2) (append (extract-add-consts v1) (extract-add-consts v2))]
+    [(vec-mul v1 v2) (append (extract-add-consts v1) (extract-add-consts v2))]
+    [(vec-div v1 v2) (append (extract-add-consts v1) (extract-add-consts v2))]
+    
+    ;; Base case
+    [_ (error "Don't know how to extract consts from:" expr)]))
+
+(define (extract-sub-consts expr)
+  (match expr
+    ;; Constructors
+    [(x32 sca) (list)]
+    [(x64 sca) (list)]
+    [(x128 sca) (list)]
+    [(ramp buf base stride len) (list)]
+
+    [(slice_vectors vec base stride len) (extract-sub-consts vec)]
+    [(concat_vectors v1 v2) (append (extract-sub-consts v1) (extract-sub-consts v2))]
+
+    ;; Type Casts
+    [(uint8x32 vec) (extract-sub-consts vec)]
+    [(uint16x32 vec) (extract-sub-consts vec)]
+    [(uint32x32 vec) (extract-sub-consts vec)]
+    [(int8x32 vec) (extract-sub-consts vec)]
+    [(int16x32 vec) (extract-sub-consts vec)]
+    [(int32x32 vec) (extract-sub-consts vec)]
+    [(uint8x64 vec) (extract-sub-consts vec)]
+    [(uint16x64 vec) (extract-sub-consts vec)]
+    [(uint32x64 vec) (extract-sub-consts vec)]
+    [(int8x64 vec) (extract-sub-consts vec)]
+    [(int16x64 vec) (extract-sub-consts vec)]
+    [(int32x64 vec) (extract-sub-consts vec)]
+    [(uint8x128 vec) (extract-sub-consts vec)]
+    [(uint16x128 vec) (extract-sub-consts vec)]
+    [(uint32x128 vec) (extract-sub-consts vec)]
+    [(int8x128 vec) (extract-sub-consts vec)]
+    [(int16x128 vec) (extract-sub-consts vec)]
+    [(int32x128 vec) (extract-sub-consts vec)]
+
+    ;; Operations
+    [(vec-add v1 v2) (append (extract-sub-consts v1) (extract-sub-consts v2))]
+    [(vec-sub v1 v2) (append
+                      (if (broadcast? v1) (extract-consts v1) (extract-sub-consts v1))
+                      (if (broadcast? v2) (extract-consts v2) (extract-sub-consts v2)))]
+    [(vec-mul v1 v2) (append (extract-sub-consts v1) (extract-sub-consts v2))]
+    [(vec-div v1 v2) (append (extract-sub-consts v1) (extract-sub-consts v2))]
+    
+    ;; Base case
+    [_ (error "Don't know how to extract consts from:" expr)]))
+
+(define (extract-mul-consts expr)
+  (match expr
+    ;; Constructors
+    [(x32 sca) (list)]
+    [(x64 sca) (list)]
+    [(x128 sca) (list)]
+    [(ramp buf base stride len) (list)]
+
+    [(slice_vectors vec base stride len) (extract-mul-consts vec)]
+    [(concat_vectors v1 v2) (append (extract-mul-consts v1) (extract-mul-consts v2))]
+
+    ;; Type Casts
+    [(uint8x32 vec) (extract-mul-consts vec)]
+    [(uint16x32 vec) (extract-mul-consts vec)]
+    [(uint32x32 vec) (extract-mul-consts vec)]
+    [(int8x32 vec) (extract-mul-consts vec)]
+    [(int16x32 vec) (extract-mul-consts vec)]
+    [(int32x32 vec) (extract-mul-consts vec)]
+    [(uint8x64 vec) (extract-mul-consts vec)]
+    [(uint16x64 vec) (extract-mul-consts vec)]
+    [(uint32x64 vec) (extract-mul-consts vec)]
+    [(int8x64 vec) (extract-mul-consts vec)]
+    [(int16x64 vec) (extract-mul-consts vec)]
+    [(int32x64 vec) (extract-mul-consts vec)]
+    [(uint8x128 vec) (extract-mul-consts vec)]
+    [(uint16x128 vec) (extract-mul-consts vec)]
+    [(uint32x128 vec) (extract-mul-consts vec)]
+    [(int8x128 vec) (extract-mul-consts vec)]
+    [(int16x128 vec) (extract-mul-consts vec)]
+    [(int32x128 vec) (extract-mul-consts vec)]
+
+    ;; Operations
+    [(vec-add v1 v2) (append (extract-mul-consts v1) (extract-mul-consts v2))]
+    [(vec-sub v1 v2) (append (extract-mul-consts v1) (extract-mul-consts v2))]
+    [(vec-mul v1 v2) (append
+                      (if (broadcast? v1) (extract-consts v1) (extract-mul-consts v1))
+                      (if (broadcast? v2) (extract-consts v2) (extract-mul-consts v2)))]
+    [(vec-div v1 v2) (append (extract-mul-consts v1) (extract-mul-consts v2))]
+    
+    ;; Base case
+    [_ (error "Don't know how to extract consts from:" expr)]))
+
+(define (extract-div-consts expr)
+  (match expr
+    ;; Constructors
+    [(x32 sca) (list)]
+    [(x64 sca) (list)]
+    [(x128 sca) (list)]
+    [(ramp buf base stride len) (list)]
+
+    [(slice_vectors vec base stride len) (extract-div-consts vec)]
+    [(concat_vectors v1 v2) (append (extract-div-consts v1) (extract-div-consts v2))]
+
+    ;; Type Casts
+    [(uint8x32 vec) (extract-div-consts vec)]
+    [(uint16x32 vec) (extract-div-consts vec)]
+    [(uint32x32 vec) (extract-div-consts vec)]
+    [(int8x32 vec) (extract-div-consts vec)]
+    [(int16x32 vec) (extract-div-consts vec)]
+    [(int32x32 vec) (extract-div-consts vec)]
+    [(uint8x64 vec) (extract-div-consts vec)]
+    [(uint16x64 vec) (extract-div-consts vec)]
+    [(uint32x64 vec) (extract-div-consts vec)]
+    [(int8x64 vec) (extract-div-consts vec)]
+    [(int16x64 vec) (extract-div-consts vec)]
+    [(int32x64 vec) (extract-div-consts vec)]
+    [(uint8x128 vec) (extract-div-consts vec)]
+    [(uint16x128 vec) (extract-div-consts vec)]
+    [(uint32x128 vec) (extract-div-consts vec)]
+    [(int8x128 vec) (extract-div-consts vec)]
+    [(int16x128 vec) (extract-div-consts vec)]
+    [(int32x128 vec) (extract-div-consts vec)]
+
+    ;; Operations
+    [(vec-add v1 v2) (append (extract-div-consts v1) (extract-div-consts v2))]
+    [(vec-sub v1 v2) (append (extract-div-consts v1) (extract-div-consts v2))]
+    [(vec-mul v1 v2) (append (extract-div-consts v1) (extract-div-consts v2))]
+    [(vec-div v1 v2) (if (broadcast? v2) (extract-consts v2) (extract-div-consts v2))]
+    
+    ;; Base case
+    [_ (error "Don't know how to extract consts from:" expr)]))
+
+(define (broadcast? vec)
+  (define v (strip-halide-casts vec))
+  (match v
+    [(x32 sca) #t]
+    [(x64 sca) #t]
+    [(x128 sca) #t]
+    [_ #f]))
+
+(define (extract-consts vec)
+  (define v (strip-halide-casts vec))
+  (match v
+    [(x32 sca) (list ((interpret-halide vec) 0))]
+    [(x64 sca) (list ((interpret-halide vec) 0))]
+    [(x128 sca) (list ((interpret-halide vec) 0))]
+    [_ (error "Don't know how to extract constant from:" v)]))
+
+(define (strip-halide-casts expr)
+  (match expr
+    [(uint8x32 vec) (strip-halide-casts vec)]
+    [(uint16x32 vec) (strip-halide-casts vec)]
+    [(uint32x32 vec) (strip-halide-casts vec)]
+    [(int8x32 vec) (strip-halide-casts vec)]
+    [(int16x32 vec) (strip-halide-casts vec)]
+    [(int32x32 vec) (strip-halide-casts vec)]
+    [(uint8x64 vec) (strip-halide-casts vec)]
+    [(uint16x64 vec) (strip-halide-casts vec)]
+    [(uint32x64 vec) (strip-halide-casts vec)]
+    [(int8x64 vec) (strip-halide-casts vec)]
+    [(int16x64 vec) (strip-halide-casts vec)]
+    [(int32x64 vec) (strip-halide-casts vec)]
+    [(uint8x128 vec) (strip-halide-casts vec)]
+    [(uint16x128 vec) (strip-halide-casts vec)]
+    [(uint32x128 vec) (strip-halide-casts vec)]
+    [(int8x128 vec) (strip-halide-casts vec)]
+    [(int16x128 vec) (strip-halide-casts vec)]
+    [(int32x128 vec) (strip-halide-casts vec)]
+    [_ expr]))
+
+(provide extract-buf-reads extract-live-ops extract-add-consts extract-sub-consts extract-mul-consts extract-div-consts)
