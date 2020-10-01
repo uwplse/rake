f7ca25b0ec7e1834676c3a9b4652dbbb1e7dff7a
Maaz Ahmad <maaz.c10@gmail.com>
2020-10-01 00:08:56 -0700

    Package meta information added

---
+++ rake/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define version "0.1")
+(define deps '("base" "rosette"))
+(define collection "rake")
+(define pkg-desc "Rake: A library for synthesizing efficient HVX implementations")
+(define pkg-authors '(Maaz Bin Safeer Ahmad))
