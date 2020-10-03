f54aff1306af2184e19e920bb5ed1c9b14f6648d
Maaz Ahmad <maaz.c10@gmail.com>
2020-10-02 17:56:49 -0700

    Added a main include file for Halide frontend

---
+++ rake/halide.rkt
@@ -0,0 +1,8 @@
+#lang rosette
+
+(require rake/halide/ir/types)
+(require rake/halide/ir/interpreter)
+
+(provide
+ (all-from-out rake/halide/ir/types)
+ (all-from-out rake/halide/ir/interpreter))
--- rake/main.rkt
+++ rake/main.rkt
@@ -1,20 +1,18 @@
 #lang rosette
 
 (require rake/util)
-(require rake/spec)
-
 (require rake/hvx/cost-model)
-
 (require rake/cpp/types)
 (require rake/cpp/cast)
 (require rake/synthesis/axioms)
 (require rake/synthesis/algorithm)
+(require rake/synthesis/spec)
 
 (provide
  debug-on debug-off init-var-types
- (all-from-out rake/spec)
  (all-from-out rake/hvx/cost-model)
  (all-from-out rake/cpp/types)
  (all-from-out rake/cpp/cast)
  (all-from-out rake/synthesis/axioms)
- (all-from-out rake/synthesis/algorithm))
+ (all-from-out rake/synthesis/algorithm)
+ (all-from-out rake/synthesis/spec))
