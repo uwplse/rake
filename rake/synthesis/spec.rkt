fce5424b6cce0b6a90fceaedda557b81862fb503
Maaz Ahmad <maaz.c10@gmail.com>
2020-10-02 17:57:11 -0700

    Moved spec file under synthesis dir

--- rake/synthesis/spec.rkt
+++ rake/synthesis/spec.rkt
@@ -1,6 +1,6 @@
 #lang rosette
 
-(struct synthesis-spec (expr context axioms))
+(struct synthesis-spec (expr axioms))
 
 (struct ir-expr-spec (expr ctx axioms buff-reads live-ops add-consts sub-consts mul-consts div-consts))
 (struct hvx-expr-spec (expr sol ctx axioms))
