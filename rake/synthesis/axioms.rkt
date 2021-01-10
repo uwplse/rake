f353f0cba8094bf3ec50a85faf031b0bbd0e86ab
Maaz Ahmad <maaz.c10@gmail.com>
2021-01-10 14:32:20 -0800

    Minor bug fix.
    Value range axioms are now inclusive on both edges. Input values have type annotations.

--- rake/synthesis/axioms.rkt
+++ rake/synthesis/axioms.rkt
@@ -7,7 +7,7 @@
   (define-symbolic idx integer?)
   (cond
     [(signed? (get buffer idx))
-     (forall (list idx) (and (bvslt (buffer idx) ub) (bvsge (buffer idx) lb)))]
+     (forall (list idx) (and (bvsle (buffer idx) (eval ub)) (bvsge (buffer idx) (eval lb))))]
     [else (error "NYI: range axiom for type ~a" (get buffer idx))]))
 
 (provide (all-defined-out))
