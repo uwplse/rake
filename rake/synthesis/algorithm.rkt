4fd812e1fd94649d4c4f0c719bc7ef2008574799
Maaz Ahmad <maaz.c10@gmail.com>
2021-04-07 01:51:36 -0700

    Unpack lower precision solutions before writing to output buffer

--- rake/synthesis/algorithm.rkt
+++ rake/synthesis/algorithm.rkt
@@ -1,5 +1,13 @@
 #lang rosette
 
+(require rake/cpp/types)
+(require rake/cpp/cast)
+
+(require rake/halide/ir/interpreter)
+
+(require rake/hvx/interpreter)
+(require rake/hvx/ast/types)
+
 (require rake/synthesis/spec)
 (require rake/synthesis/ir-expr-synthesizer)
 (require rake/synthesis/hvx-expr-synthesizer)
@@ -20,6 +28,8 @@
         ;; Synthesize equivalent HVX expression
         (define-values (successful? hvx-expr)
           (synthesize-hvx-expr halide-expr halide-expr-axioms ir-expr ir-expr-sol ir-annotations lowering-algo swizzling-algo))
+
+        (set! hvx-expr (unpack-if-needed hvx-expr halide-expr))
 
         (define verifies? #f)
         (when successful?
@@ -65,5 +75,19 @@
        arm-expr)]
 
     [else (error (format "Input specification is provided in a language Rake currently does not support: '~a. Supported IRs: ['halide-ir]" spec-lang))]))
+
+(define (unpack-if-needed hvx-expr halide-expr)
+  (define bw-o-type (bw (type ((interpret-halide halide-expr) 0))))
+  (define bw-s-type (bw (elem-type (hvx-type (interpret-hvx hvx-expr)))))
+  (cond
+    [(eq? bw-o-type bw-s-type) hvx-expr]
+    [(eq? bw-o-type (* 2 bw-s-type))
+     (define unpacked-hvx-expr
+       (cond
+         [(hvx-pair? (interpret-hvx hvx-expr)) (let-expr 'v0 hvx-expr (list (vunpack (lo 'v0)) (vunpack (hi 'v0))))]
+         [else (vunpack 'v0)]))
+     unpacked-hvx-expr]
+    [else
+     (error "NYI: Expression unpacking when the required type is more than twice as wide.")]))
 
 (provide synthesize-hvx synthesize-arm)
