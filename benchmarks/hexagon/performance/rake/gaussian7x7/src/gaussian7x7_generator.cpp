7547ed469fe1c5d0a52d5608cca4b584eeba0678
Maaz Ahmad <maaz.c10@gmail.com>
2021-04-08 11:53:24 -0700

    Minor

--- benchmarks/hexagon/performance/rake/gaussian7x7/src/gaussian7x7_generator.cpp
+++ benchmarks/hexagon/performance/rake/gaussian7x7/src/gaussian7x7_generator.cpp
@@ -53,7 +53,6 @@ public:
             rows.compute_at(Func(output), y)
                 .tile(x, y, x, y, xi, yi, vector_size, 4, TailStrategy::RoundUp)
                 .vectorize(xi)
-                .align_storage(x, vector_size)
                 .unroll(yi);
 
             output.prefetch(input, y, 2, PrefetchBoundStrategy::NonFaulting);
