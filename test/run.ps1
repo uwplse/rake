f548b62a23d9f6165f9c9e6ce5cccefdf56b0aee
Maaz Ahmad <maaz.c10@gmail.com>
2021-05-05 22:30:45 -0700

    Updated test scripts now print runtimes

--- test/run.ps1
+++ test/run.ps1
@@ -1,15 +1,26 @@
+$tblFile = "result_table.csv"
+if (Test-Path $tblFile) {
+  Remove-Item $tblFile
+}
+
 Get-ChildItem $args[0] -Filter *.rkt | 
 Foreach-Object {
 	$bmName = Split-Path $_.DirectoryName -Leaf
 	$rktFile = $_
 	$filedir = join-path $_.DirectoryName $_.BaseName
 	$logFile = "$($filedir)_log.txt"
-	Racket.exe $_.FullName | Out-File -FilePath $logFile
-	if ( $LASTEXITCODE -eq 0 )
+	$exitcode = 0
+	$runtime = Measure-Command {
+		Racket.exe $_.FullName | Out-File -FilePath $logFile
+		$exitcode = $LASTEXITCODE
+	}
+	if ( $exitcode -eq 0 )
 	{
-	    "$($bmName), $($rktFile), passed" | Out-Default
+	    "$($bmName), $($rktFile), passed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-Default
+	    "$($bmName), $($rktFile), passed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-File -Append -FilePath $tblFile
 	}
 	else {
-		"$($bmName), $($rktFile), failed" | Out-Default
+		"$($bmName), $($rktFile), failed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-Default
+		"$($bmName), $($rktFile), failed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-File -Append -FilePath $tblFile
 	}
 }
--- test/runall.ps1
+++ test/runall.ps1
@@ -1,15 +1,26 @@
+$tblFile = "result_table.csv"
+if (Test-Path $tblFile) {
+  Remove-Item $tblFile
+}
+
 Get-ChildItem *.rkt -Recurse | 
 Foreach-Object {
 	$bmName = Split-Path (Split-Path $_ -Parent) -Leaf
 	$rktFile = $_.Name
 	$filedir = join-path $_.DirectoryName $_.BaseName
 	$logFile = "$($filedir)_log.txt"
-	Racket.exe $_ | Out-File -FilePath $logFile
-	if ( $LASTEXITCODE -eq 0 )
+	$exitcode = 0
+	$runtime = Measure-Command {
+		Racket.exe $_ | Out-File -FilePath $logFile
+		$exitcode = $LASTEXITCODE
+	}	
+	if ( $exitcode -eq 0 )
 	{
-	    "$($bmName), $($rktFile), passed" | Out-Default
+	    "$($bmName), $($rktFile), passed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-Default
+	    "$($bmName), $($rktFile), passed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-File -Append -FilePath $tblFile
 	}
 	else {
-		"$($bmName), $($rktFile), failed" | Out-Default
+		"$($bmName), $($rktFile), failed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-Default
+		"$($bmName), $($rktFile), failed, $([math]::ceiling($runtime.TotalSeconds))s" | Out-File -Append -FilePath $tblFile
 	}
 }
