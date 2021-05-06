65cd23c3d94a80388e3a2ad9bda29ffb3b1e3fea
Maaz Ahmad <maaz.c10@gmail.com>
2021-05-05 22:30:35 -0700

    New script to generate table in GitHub markdown from csv

---
+++ test/csv_to_markdown.py
@@ -0,0 +1,50 @@
+import csv
+import sys
+
+def emoji(res):
+	if res == "passed":
+		return ":heavy_check_mark:"
+	else:
+		return ":x:".ljust(18)
+
+with open(sys.argv[1], encoding="utf16") as csvfile:
+	result_tbl = csv.reader(csvfile, delimiter=',')
+
+	header  = "| ID  | Benchmark Name  | Spec File   | Can lift?          | Can sketch?        | Can swizzles?      | Can verify?        |"
+	divider = "| :-: | :-------------- | :---------- | :----------------: | :----------------: | :----------------: | :----------------: |"
+	
+	print (header)
+	print (divider)
+
+	row_id = 1
+	for row in result_tbl:
+		row = [val.strip() for val in row]
+		tbl_row = "| " + str(row_id).ljust(3) + " | " +	\
+			 row[0].ljust(15) + " | " + \
+			 row[1].ljust(11) + " | " + \
+			 emoji(row[2]) + \
+			 " | :x:                | :x:                | :x:                |"
+		print (tbl_row)
+		row_id = row_id + 1
+
+print ("\n\n")
+
+with open(sys.argv[1], encoding="utf16") as csvfile:
+	result_tbl = csv.reader(csvfile, delimiter=',')
+
+	header  = "| ID  | Benchmark Name  | Spec File   | Lift time |"
+	divider = "| :-: | :-------------- | :---------- | :-------: |"
+	
+	print (header)
+	print (divider)
+
+	row_id = 1
+	for row in result_tbl:
+		row = [val.strip() for val in row]
+		runtime = row[3].rjust(9) if row[2] == "passed" else "-".rjust(9)
+		tbl_row = "| " + str(row_id).ljust(3) + " | " +	\
+			 row[0].ljust(15) + " | " + \
+			 row[1].ljust(11) + " | " + \
+			 runtime + " |"
+		print (tbl_row)
+		row_id = row_id + 1
