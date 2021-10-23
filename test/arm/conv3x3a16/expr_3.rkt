4f54a5840a4fb7dbcff6910238b42b35a672235b
Maaz Ahmad <maaz.c10@gmail.com>
2021-10-23 11:57:00 -0700

    Con3x3a16 benchmark added for ARM

---
+++ test/arm/conv3x3a16/expr_3.rkt
@@ -0,0 +1,26 @@
+#lang rosette/safe
+
+(require rake)
+(init-logging "expr_3.runtimes")
+
+(define-symbolic-var input_bounded.s0.x.x int32_t)
+(define-symbolic-var t111 int32_t)
+(define-symbolic-var input.extent.0 int32_t)
+
+(define axioms 
+  (list ))
+
+
+(define halide-expr
+ (vec-add
+  (vec-max
+   (vec-min
+    (ramp (sca-add (sca-mul input_bounded.s0.x.x (int32_t (bv 16 32))) (int32_t (bv -1 32))) (int32_t (bv 1 32)) 16)
+    (x16 (sca-add input.extent.0 (int32_t (bv -1 32)))))
+   (x16 (int32_t (bv 0 32))))
+  (x16 t111)))
+
+(define spec (synthesis-spec 'halide-ir halide-expr axioms))
+(define arm-expr (synthesize-arm spec 'greedy 'enumerative 'enumerative))
+
+(llvm-codegen arm-expr "sexp_3.out")
