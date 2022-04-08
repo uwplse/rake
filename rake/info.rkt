f88980dffe0544304c3d299367727e4e1aa84f73
Maaz Ahmad <maaz.c10@gmail.com>
2022-04-08 16:38:12 -0700

    Update info.rkt

--- rake/info.rkt
+++ rake/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
-(define version "0.2")
+(define version "0.3")
 (define deps '("base" "rosette"))
 (define collection "rake")
-(define pkg-desc "Rake: A library for synthesizing efficient HVX and ARM implementations")
+(define pkg-desc "Rake: A library for synthesizing efficient HVX, ARM and AVX2 implementations")
 (define pkg-authors '("Maaz Bin Safeer Ahmad" "Alex J. Root"))
